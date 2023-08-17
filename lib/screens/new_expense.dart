3b6d528a1ea31f9d35301130e61d612d9e93de8b
Redhun <redhuns123@gmail.com>
2023-08-17 22:45:41 +0530

    date picker added

--- lib/screens/new_expense.dart
+++ lib/screens/new_expense.dart
@@ -1,3 +1,4 @@
+import 'package:expensetracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -8,25 +9,92 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle= '';
-  void _saveTitle(String title){
-     _enteredTitle=title;
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  void _datePicker() async {
+    final now = DateTime.now();
+    final fristDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: fristDate,
+        lastDate: now);
+    setState(() {
+      _selectedDate = pickedDate;
+    });
   }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Padding(padding: const EdgeInsets.all(16),
-    child: Column(children: [
-      TextField(
-        onChanged: _saveTitle,
-        maxLength: 50,
-        decoration:const  InputDecoration(
-          label: Text('title'),
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              label: Text('title'),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  controller: _amountController,
+                  maxLength: 50,
+                  decoration: const InputDecoration(
+                    prefixText: '\$',
+                    label: Text('amount'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(_selectedDate == null
+                        ? 'select date'
+                        : formatter.format(_selectedDate!)),
+                    IconButton(
+                      onPressed: _datePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('cancel'),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: const Text('save'))
+            ],
+          )
+        ],
       ),
-      Row(children: [
-        ElevatedButton(onPressed:(){ print(_enteredTitle);}, child: const Text('save'))
-      ],)
-    ],),
     );
   }
 }
